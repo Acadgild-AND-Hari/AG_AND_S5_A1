4ae7003f704424d61d22e744327460e90858f09f
harinivaskumar <harinivaskumar@live.com>
2016-12-14 05:12:39 +0530

    Code Commit
    Initial code commit

---
+++ app/src/main/java/com/hari/aag/textcolorchanger/TextColorChangerActivity.java
@@ -0,0 +1,13 @@
+package com.hari.aag.textcolorchanger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TextColorChangerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_color_changer);
+    }
+}
